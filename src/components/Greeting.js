18126ec25e894c28f13ed5f0f1bbb0d410074670
paalss <paalss94@gmail.com>
2021-07-28 16:13:33 +0200

    flere tester, gruppert i suite, tester interaksjoner og false positives

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -1,8 +1,17 @@
+import { useState } from "react";
+
 const Greeting = () => {
+  const [changeText, setChangeText] = useState(false);
+  const changeTextHandler = () => {
+    setChangeText(true);
+  };
   return (
     <>
       <h2>Hello world</h2>
       <p>rrrrrrrr</p>
+      {!changeText && <p>nooooo</p>}
+      {changeText && <p>klikket!</p>}
+      <button onClick={changeTextHandler}>change text</button>
     </>
   );
 };
--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -1,15 +1,50 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Greeting from "./Greeting";
 
-// test er globally available
-test("renders hello world tekst", () => {
-  // Arrange
-  render(<Greeting />);
+// suite
+describe("Greeting compoent", () => {
+  // test er globally available
+  test("renders hello world tekst", () => {
+    // Arrange
+    render(<Greeting />);
 
-  // Act
-  //
+    // Act
+    //
 
-  // Assert
-  const helloWorldElement = screen.getByText("hello world", { exact: false });
-  expect(helloWorldElement).toBeInTheDocument();
+    // Assert
+    const helloWorldElement = screen.getByText("hello world", { exact: false });
+    expect(helloWorldElement).toBeInTheDocument();
+  });
+
+  test("renders text if button not clicked", () => {
+    // Arrange
+    render(<Greeting />);
+    // Asserting
+    const text = screen.getByText(/nooooo/i);
+    expect(text).toBeInTheDocument();
+  });
+
+  test("rendrer 'klikket!' etter button click", () => {
+    // arrange
+    render(<Greeting />);
+
+    // act
+    const buttonElement = screen.getByRole("button");
+    userEvent.click(buttonElement);
+
+    // assert
+    const text = screen.getByText("klikket!", { exact: false });
+    expect(text).toBeInTheDocument();
+  });
+
+  // teste false positives
+  test("rendrer ikke 'klikket!' før button click", () => {
+    // arrange
+    render(<Greeting />);
+
+    // assert
+    const text = screen.queryByText("klikket!", { exact: false });
+    expect(text).toBeNull();
+  });
 });
